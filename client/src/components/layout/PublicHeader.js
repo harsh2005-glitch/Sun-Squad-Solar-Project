45b623de415234d87e8c7a6e6fbceb788814a2b2
harsh2005-glitch <harshitmaurya849@gmail.com>
2025-10-08 00:49:36 +0530

    navbar fixed for mobile

--- client/src/components/layout/PublicHeader.js
+++ client/src/components/layout/PublicHeader.js
@@ -1,31 +1,51 @@
 import React from 'react';
+// Import Link from react-router-dom to handle internal navigation
 import { Link } from 'react-router-dom';
-import logo from '../../assets/images/logo.png'; // Import the logo image
+// Import Bootstrap components
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import logo from '../../assets/images/logo.png';
 
-// Note: The enquiry modal logic will be passed in as a prop later
 const PublicHeader = ({ onEnquiryClick }) => {
   return (
-    <header id="main-header">
-      
-      <nav className="main-nav">
-        <div className="logo">
-          <Link to="/"><img src={logo} alt="Company Logo" /></Link>
-        </div>
-        <ul className="nav-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About Us</Link></li>
-          <li><Link to="/locations">Our Project Location</Link></li>
-          <li><Link to="/gallery">Gallery</Link></li>
-          <li><Link to="/announcements">Announcement</Link></li>
-          <li><Link to="/contact">Contact Us</Link></li>
-        </ul>
-        <div className="nav-buttons">
-          <button onClick={onEnquiryClick} className="btn btn-enquiry">GET AN ENQUIRY</button>
-          {/* This now links to our React login page */}
-          <Link to="/login" className="btn btn-login">LOGIN</Link>
-        </div>
-      </nav>
-    </header>
+    // `sticky="top"` makes the navbar stay at the top when scrolling
+    <Navbar bg="light" expand="lg" sticky="top" className="shadow-sm">
+      <Container>
+        {/* Logo */}
+        <Navbar.Brand as={Link} to="/">
+          <img
+            src={logo}
+            height="50"
+            className="d-inline-block align-top"
+            alt="Sun Squad Solar Logo"
+          />
+        </Navbar.Brand>
+
+        {/* Hamburger Menu Toggle Button (appears on mobile) */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        {/* Collapsible section */}
+        <Navbar.Collapse id="basic-navbar-nav">
+          {/* `ms-auto` pushes the nav links to the right */}
+          <Nav className="ms-auto align-items-center">
+            {/* Use `as={Link}` to make Nav.Link behave like a React Router Link */}
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About Us</Nav.Link>
+            <Nav.Link as={Link} to="/locations">Our Project Location</Nav.Link>
+            <Nav.Link as={Link} to="/gallery">Gallery</Nav.Link>
+            <Nav.Link as={Link} to="/announcements">Announcement</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link>
+
+            {/* Buttons */}
+            <Button variant="outline-primary" onClick={onEnquiryClick} className="mx-2 my-2 my-lg-0">
+              GET AN ENQUIRY
+            </Button>
+            <Button as={Link} to="/login" variant="dark">
+              LOGIN
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
